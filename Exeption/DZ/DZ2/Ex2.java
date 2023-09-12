efcff96e6a0f30930eb12eb2d5fdb886699f3022
valery tryapitsyn <tryapitsyn@healbe.com>
2023-09-12 10:30:47 +0300

    DZ2

---
+++ Exeption/DZ/DZ2/Ex2.java
@@ -0,0 +1,18 @@
+//Если необходимо, исправьте данный код https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
+public class Ex2 {
+    public static void main(String[] args) {
+        foo1();
+    }
+
+    public static void foo1(){
+        try {
+            int[] intArray = {1,2,3,4,5,6,7,8,9};
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
